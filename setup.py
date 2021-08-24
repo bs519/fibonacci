9fc5ced21d5ae2a0d3c1b56281264006933167e7
bs519 <baptiste.simandoux19@imperial.ac.uk>
2021-08-24 16:58:26 +0100

    small change

--- setup.py
+++ setup.py
@@ -1,2 +1,7 @@
 from setuptools import setup, find_packages, version
-setup(name="fibonacci", version="0.1", packages=find_packages())
+
+setup(
+    name="fibonacci",
+    version="0.1",
+    packages=find_packages()
+)
